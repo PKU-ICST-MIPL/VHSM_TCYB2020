6964f64139425e5fe03b699c989f90348dcce32b
Zd Ye <pengyuxin@pku.edu.cn>
2020-10-01 14:10:25 +0800

    delete_old_version

--- my/base_module/__init__.py
+++
@@ -1,1 +0,0 @@
-from my.base_module.general import *
--- run.sh
+++
@@ -1,4 +0,0 @@
-CUDA_VISIBLE_DEVICES=1  python train.py  --model_dir ./logs/VHSM   --batch_size 100 --max_epochs 25 --max_sent_length 30 --learning_rate 0.001
-
-
-
